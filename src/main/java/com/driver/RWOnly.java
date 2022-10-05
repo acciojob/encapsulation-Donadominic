e25d92c608bb8e96bd508e0f9fb7142ea247538d
Donadominic <43172188+Donadominic@users.noreply.github.com>
2022-10-05 11:54:47 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int var1;
+    public int getVaue() {
+        return var1;
+    }
+    public void setValue(int var1) {
+        this.var1=var1;
+        return;
+    }
 }
